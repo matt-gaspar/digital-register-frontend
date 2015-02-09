65022c993203d2731996d1d3c2264c38a0eff1da
Camille Baldock <camille@camillebaldock.co.uk>
2015-02-09 10:02:11 +0000

    API mocking for tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,46 @@
 from service.server import app
-import unittest
 import json
-
 import mock
 import unittest
 import requests
 import responses
+
+from .fake_response import FakeResponse
+
+fake_title = FakeResponse(b'''{
+    "address": "5 Granary Avenue Poundbury Dorchester DT1 4YY",
+    "coordinates": {
+        "latitude": 45,
+        "longitude": 45
+    },
+    "last_changed": "2014-05-22 15:39:52",
+    "lenders": [
+        {
+        "name": "Santander"
+        }
+    ],
+    "number": "DT122047",
+    "proprietors": [
+        {
+        "address": "5 Granary Avenue Poundbury Dorchester DT1 4YY",
+        "name": "Raymond Frank Easton"
+        },
+        {
+        "address": "5 Granary Avenue Poundbury Dorchester DT1 4YY",
+        "name": "Carol Easton"
+        }
+    ],
+    "tenure_type": "Leasehold"
+    }'''
+)
+
 
 class ViewTitleTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
 
-    def test_get_title_page(self):
+    @mock.patch('requests.get', return_value=fake_title)
+    def test_get_title_page(self, mock_get):
         response = self.app.get('/titles/titleref')
         assert response.status_code == 200
