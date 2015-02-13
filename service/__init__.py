0d4431d2aab6650aaa652737182031345f661136
Pheonnexx <pheonn@hotmail.co.uk>
2015-02-13 16:01:25 +0000

    added/working on property address

--- service/__init__.py
+++ service/__init__.py
@@ -1,14 +1,14 @@
 import os, logging
 from flask import Flask
-import time
-from time import strftime
+import dateutil
+import dateutil.parser
 
 import requests
 
 app = Flask(__name__)
 
 def format_datetime(value):
-  return strftime("%d %b %Y at %H:%M:%S", time.strptime(value, "%Y-%m-%d %H:%M:%S"))
+  return dateutil.parser.parse(value).strftime("%d %b %Y at %H:%M:%S")
 
 app.config.from_object(os.environ.get('SETTINGS'))
 app.jinja_env.filters['datetime'] = format_datetime
--- service/server.py
+++ service/server.py
@@ -23,6 +23,7 @@ def display_title(title_ref):
     }
     entries = title_api['data']['entries']
     proprietor_names = []
+    first_line_address = []
     for entry in entries:
         if entry['role_code'] == 'RPRO':
             infills = entry['infills']
@@ -30,9 +31,15 @@ def display_title(title_ref):
                 proprietors = infill['proprietors']
                 for proprietor in proprietors:
                     proprietor_names += [proprietor['name']['forename'] + ' ' + proprietor['name']['surname']]
+        if entry['role_code'] == 'RDES':
+            infills = entry['infills']
+            for infill in infills:
+                address_part = infill['address']
+                first_line_address.append(address_part['house_no'] + ' ' + address_part['street_name'])
+                property_description = {'first_line_address': first_line_address, 'town': address_part['town'], 'postcode': address_part['postcode'] }
 
     return render_template('display_title.html', asset_path = '../static/',
-                           title=title, proprietors=proprietor_names)
+                           title=title, proprietors=proprietor_names, property_description=property_description)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
