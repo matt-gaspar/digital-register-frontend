64c30c1b6fa79e899bc7be961ce0dbedc0c5cf01
daveogorman <david.ogorman@digital.landregistry.gov.uk>
2015-02-16 15:10:58 +0000

    US024 acceptance tests for single or multiple proprietors

--- acceptance-tests/features/step_definitions/digital_register_frontend_steps.rb
+++ acceptance-tests/features/step_definitions/digital_register_frontend_steps.rb
@@ -15,9 +15,13 @@ Given(/^I have a property$/) do
     :postcode => "PL9 BLT",
     :street_name => "Test Street",
     :house_no => 13,
-    :town => "Plymouth"
+    :town => "Plymouth",
+    :surname => "Marie",
+    :forename => "Hill",
+    :name_category => "Personal",
+    :full_text => "PROPRIETOR: %MARIE HILL% of Flat 113, Eaton Rise, Eton College Road, *London* NW3 2DD.",
   }
-  create_title_in_db(@property_hash)
+  create_proprietor_title_in_db(@property_hash)
 end
 
 Given(/^I do not have a property$/) do
@@ -47,4 +51,58 @@ end
 
 Then(/^I get a page not found message$/) do
   expect(page.status_code).to eq(404)
+end
+
+
+#*************************************
+Given(/^I have a property owned by an individual$/) do
+  # empty the database
+  delete_all_titles
+  # insert the property_hash data into the database
+  @property_hash = {
+    :title_number => "DN1000",
+    :postcode => "NW3 2DD",
+    :street_name => "Eaton Rise",
+    :house_no => 113,
+    :town => "London",
+    :surname => "Hill",
+    :forename => "Marie",
+    :name_category => "Personal",
+    :full_text => "PROPRIETOR: %MARIE HILL% of Flat 113, Eaton Rise, Eton College Road, *London* NW3 2DD.",
+    :multi_proprietors => "singlePI"
+  }
+  create_proprietor_title_in_db(@property_hash)
+end
+
+Then(/^I can see who owns the property$/) do
+  content = page.body.text
+  expect(content).to include("#{@property_hash[:forename]} #{@property_hash[:surname]}")
+end
+
+Given(/^the property is owned by multiple individuals$/) do
+  # empty the database
+  delete_all_titles
+  # insert the property_hash data into the database
+  @property_hash = {
+    :title_number => "DN1000",
+    :postcode => "NW3 2DD",
+    :street_name => "Eaton Rise",
+    :house_no => 113,
+    :town => "London",
+    :surname => "Hicks",
+    :forename => "Fred",
+    :name_category => "Personal",
+    :full_text => "PROPRIETOR: %FRED HICKS% of Flat 113, Eaton Rise, Eton College Road, *London* NW3 2DD.",
+    :multi_proprietors => "TwoPI"
+  }
+  create_proprietor_title_in_db(@property_hash)
+end
+
+Then(/^I can see all the owners the property$/) do
+  #pending # express the regexp above with the code you wish you had
+  content = page.body.text
+  expect(content).to include("#{@property_hash[:forename]} #{@property_hash[:surname]}")
+  #expect(content).to include("#{@property_hash[:forename2]} #{@property_hash[:surname2]}")
+  #expect(content).to include("#{@property_hash[:forename3]} #{@property_hash[:surname3]}")
+  #expect(content).to include("#{@property_hash[:forename4]} #{@property_hash[:surname4]}")
 end
