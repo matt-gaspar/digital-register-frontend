d185a54012125b5c8655074042166dde2c6a1b5e
Pheonnexx <pheonn@hotmail.co.uk>
2015-02-17 18:32:08 +0000

    amended dealing with address and added appropriate unit tests

--- service/server.py
+++ service/server.py
@@ -19,6 +19,8 @@ def display_title(title_ref):
         entry_groups = title_api['data']['groups']
         #This is to get the proprietor entry that is in the ownership category
         proprietor_names = []
+        property_description = {}
+        address_lines = []
         for entry_group in entry_groups:
             if entry_group['category'] == 'OWNERSHIP':
                 proprietor_entries = (entry_group['entries'])
@@ -36,23 +38,23 @@ def display_title(title_ref):
             #This is to get the proprietor entry that is in the ownership category
             if entry_group['category'] == 'PROPERTY':
                 property_entries = (entry_group['entries'])
-                property_description = {}
                 for entry in property_entries:
                     if entry['role_code'] == 'RDES':
                         infills = entry['infills']
                         for infill in infills:
                             address_part = infill['address']
                             if address_part:
-                                first_line_address = address_part['house_no'] + ' ' + address_part['street_name']
-                                property_description = {
-                                    'first_line': first_line_address,
-                                    'town': address_part['town'],
-                                    'postcode': address_part['postcode']
-                                }
+                                first_line_address = ' '.join([address_part[k] for k in ['house_no', 'street_name'] if address_part.get(k, None)])
+                                all_address_lines = [
+                                    first_line_address,
+                                    address_part.get('town', ''),
+                                    address_part.get('postcode', '')
+                                ]
+                                address_lines = [line for line in all_address_lines if line]
         title = {
             'number': title_api['title_number'],
             'last_changed': title_api['data']['last_app_timestamp'],
-            'address': property_description,
+            'address_lines': address_lines,
             'lenders': [
                 {'name': 'TODO lender name'},
             ],
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,10 +7,22 @@ import responses
 
 from .fake_response import FakeResponse
 
-fake_title_json_file = open('tests/fake_title.json', 'r')
-fake_title_json_string = fake_title_json_file.read()
-fake_title_bytes = str.encode(fake_title_json_string)
-fake_title = FakeResponse(fake_title_bytes)
+with open('tests/fake_title.json', 'r') as fake_title_json_file
+    fake_title_json_string = fake_title_json_file.read()
+    fake_title_bytes = str.encode(fake_title_json_string)
+    fake_title = FakeResponse(fake_title_bytes)
+
+with open('tests/fake_no_address_title.json', 'r') as fake_no_address_title_file:
+    fake_no_address_title_json_string = fake_no_address_title_file.read()
+    fake_no_address_title_bytes = str.encode(fake_no_address_title_json_string)
+    fake_no_address_title = FakeResponse(fake_no_address_title_bytes)
+
+with open('tests/fake_partial_address.json', 'r') as fake_partial_address_file:
+    fake_partial_address_json_string = fake_partial_address_file.read()
+    fake_partial_address_bytes = str.encode(fake_partial_address_json_string)
+    fake_partial_address = FakeResponse(fake_partial_address_bytes)
+
+
 
 class ViewTitleTestCase(unittest.TestCase):
 
@@ -33,6 +45,18 @@ class ViewTitleTestCase(unittest.TestCase):
         self.assertIn('17 Hazelbury Crescent', str(response.data))
         self.assertIn('Luton', str(response.data))
         self.assertIn('LU1 1DZ', str(response.data))
+
+    @mock.patch('requests.get', return_value=fake_partial_address)
+    def test_partial_address_on_title_page(self, mock_get):
+        response = self.app.get('/titles/titleref')
+        self.assertIn('Hazelbury Crescent', str(response.data))
+        self.assertIn('Luton', str(response.data))
+        self.assertIn('LU1 1DZ', str(response.data))
+
+    @mock.patch('requests.get', return_value=fake_no_address_title)
+    def test_not_available_when_no_address_on_title_page(self, mock_get):
+        response = self.app.get('/titles/titleref')
+        self.assertIn('Not Available', str(response.data))
 
     @mock.patch('requests.get', return_value=fake_title)
     def test_proprietor_on_title_page(self, mock_get):
