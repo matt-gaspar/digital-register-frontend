2b0e71edee20f6ced638ac97f765288afb941702
Leszek Gonczar <gonczar@gmail.com>
2015-03-06 15:07:53 +0000

    Setup logging

--- run_dev.py
+++ run_dev.py
@@ -2,4 +2,4 @@
 
 from service.server import app
 import os
-app.run(host="0.0.0.0", port=int(os.environ['PORT']), debug=True)
+app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8003)), debug=True)
