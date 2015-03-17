393bc31aae90e81025652b9b55ed36b25bca5378
Aron Jasper <aron.jasper@digital.landregistry.gov.uk>
2015-03-17 09:14:06 +0000

    Add Google Analytics to all pages

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ CONFIG_DICT = {
     'DEBUG': False,
     'REGISTER_TITLE_API': 'http://landregistry.local:8004/',
     'LOGGING_CONFIG_FILE_PATH': 'logging_config.json',
-    'GOOGLE_ANALYTICS_API_KEY': 'Test',
+    'GOOGLE_ANALYTICS_API_KEY': 'UA-59849906-2',
 }
 
 settings = os.environ.get('SETTINGS')
