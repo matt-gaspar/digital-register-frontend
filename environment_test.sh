5268a64657486a814ee6ae01b193c290fd3c7e71
Camille Baldock <camille@camillebaldock.co.uk>
2015-03-19 16:35:55 +0000

    All environment variables as os variables

--- environment_test.sh
+++ environment_test.sh
@@ -1,1 +1,4 @@
 export SETTINGS='test'
+export REGISTER_TITLE_API='http://landregistry.local:8004/'
+export LOGGING_CONFIG_FILE_PATH='logging_config.json'
+export GOOGLE_ANALYTICS_API_KEY='UA-59849906-2'
